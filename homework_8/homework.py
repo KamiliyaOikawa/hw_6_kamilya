a2a62db3627e0a3530206bcc266c9dd49010229f
KamiliyaOikawa <knasyrova622@gmail.com>
2022-01-29 21:05:03 +0600

    IloveOikawa

--- homework_8/homework.py
+++ homework_8/homework.py
@@ -1,4 +1,5 @@
 import sqlite3
+
 d_b = sqlite3.connect("Plan.db")
 sql = d_b.cursor()
 sql.execute(
@@ -22,7 +23,7 @@ def registr():
         sql.execute(f"SELECT Day FROM users WHERE DAY = '{day_week}'")
         if sql.fetchone() is None:
             sql.execute(f"INSERT INTO users VALUES (?,?,?,?)",
-                        ( day_week, plan_morning, plan_lunch, plan_evening))
+                        (day_week, plan_morning, plan_lunch, plan_evening))
             d_b.commit()
             for value in sql.execute("SELECT * FROM users"):
                 print(value)
@@ -33,25 +34,22 @@ def registr():
         delete_choose = input("Do you have any fulfilled days?"
                               "If there is, then write the day of the week, if not, then write no:")
         sql.execute("DELETE FROM users WHERE Day == ?", (delete_choose,))
-        if delete_choose != "нет":
+        if delete_choose != "n":
             for value in sql.execute("SELECT * FROM users"):
                 print(value)
             continue
         choose = input("Do you want to continue recording your plans? Yes or no")
-        if choose == "Yes":
+        if choose == "y":
             continue
-        elif choose == "No":
+        elif choose == "n":
             for value in sql.execute("SELECT * FROM users"):
                 print(value)
             print("Your plans are registered!")
             break
         else:
             print("Write only yes or no")
-
             break
 
 
 if __name__ == '__main__':
     registr()
-
-
